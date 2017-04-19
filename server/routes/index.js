5e4b6034142c4578e7828d7cf26a5f68b46fe1ef
chenkang1 <chenkang1@lenovo.com>
2017-04-19 11:11:04 +0800

    change the route module

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,28 @@
-module.exports = function (app) {
-    app.use('/', require('./page1'));
-    app.use('/page2', require('./page2'));
-};
+// let routers = function routers(app) {
+
+//     app.get('/', function(req, res, next) {
+//         res.render('index', { title: 'Express' });
+//     });
+
+//     app.get('/getData', function(req, res, next) {
+//         res.send({ title: 'Express' });
+//     });
+
+
+//     app.post('/api/v1/auth/login', function(req, res, next) {
+//         res.send({ "message": "Logged in" });
+//     });
+
+//     app.get('/api/v1/users/me', function(req, res, next) {
+//         res.send({"username":"admin","email":"admin@localhost","role":"admin","groups":[],"type":0,"status":true,"firstname":"","lastname":"","notificationenabled":false});
+//     });
+
+// }
+
+// module.exports = routers;
+
+module.exports = function routes(app){
+
+    require("./overview.route.js")(app)
+
+}
